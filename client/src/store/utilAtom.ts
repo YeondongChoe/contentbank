4cdad6938e28356eefc8fb0c2483f5b76a698852
hyoseon <hskim1@olympiad.ac>
2024-01-26 15:20:13 +0900

    네비 구조 시멘틱하게 변경

--- client/src/store/utilAtom.ts
+++ client/src/store/utilAtom.ts
@@ -1,5 +1,6 @@
 import { atom } from 'recoil';
 
+// 페이지네이션 공통아톰
 export const totalPageAtom = atom({
   key: 'totalPageAtom',
   default: 1,
@@ -10,6 +11,7 @@ export const pageAtom = atom({
   default: 1,
 });
 
+// 메뉴슬라이드 아톰
 export const openNaviationBoolAtom = atom({
   key: 'openNaviationBoolAtom',
   default: true,
