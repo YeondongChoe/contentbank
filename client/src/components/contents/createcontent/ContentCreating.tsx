9f949af39ab75d6f3b4dce0626e145b207b4aef5
hyoseon <hskim1@olympiad.ac>
2024-02-27 16:27:47 +0900

    문항 등록 셀렉트 로직

--- client/src/components/contents/createcontent/ContentCreating.tsx
+++ client/src/components/contents/createcontent/ContentCreating.tsx
@@ -3,120 +3,17 @@ import { useEffect, useMemo, useState } from 'react';
 
 import styled from 'styled-components';
 
-import {
-  Button,
-  Modal,
-  OptionsDepsProps,
-  OptionsItemProps,
-  Select,
-} from '../..';
-import { useModal } from '../../../hooks';
+import { Button, Modal, Select } from '../..';
 import { COLOR } from '../../constants/COLOR';
 
-import {
-  selectCategory1,
-  selectCategory2,
-  selectCategory3,
-} from './contentCreatingCategory';
-import { SchoolInputModal } from './SchoolInputModal';
+import { selectCategory1, selectCategory3 } from './contentCreatingCategory';
+import { OptionList } from './options/OptionList';
 
 export function ContentCreating() {
-  const { openModal } = useModal();
-  // const [content, setContent] = useState<string[]>([]);
-  const [sourceOptions, setSourceOptions] = useState<number[]>([0]);
-  const [count, setCount] = useState(1);
-  const [selectValue, setSelectValue] = useState({
-    idx: 0,
-    value: '',
-  });
-  const [optionList, setOptionList] = useState<
-    { idx: number; label: string; options: OptionsItemProps[] }[]
-  >([]);
-
   const selectCategoryOption = (event: React.MouseEvent<HTMLButtonElement>) => {
     const value = event.currentTarget.value;
-    const id =
-      event.currentTarget.parentElement?.parentElement?.parentElement
-        ?.parentElement?.parentElement?.parentElement?.id;
 
     // setContent((prevContent) => [...prevContent, value]);
-
-    // 셀렉트 선택이후 옵션에 속한 버튼값보여주기
-    setSelectValue({ idx: Number(id), value: value });
-
-    // 셀렉트 선택이후 셀렉트에 속한 자식 배열값 보여주기
-  };
-
-  const showOptionList = () => {
-    const arr = selectCategory2[0]?.options.filter(
-      (el) => el.label === selectValue.value,
-    );
-    // arr[0] && console.log(arr[0], arr[0].optionsDeps, selectValue.idx);
-    // console.log(selectValue.value);
-
-    if (arr[0] && arr[0].optionsDeps) {
-      // 이미 선택된 셀렉트 값일 경우 중복선택 불가
-      const value = optionList.filter((el) => el.label === selectValue.value);
-
-      // 인덱스가 같은 셀렉트에서 재선택시 덮어씌우기
-      const arrUnique = optionList.filter(
-        (arr, index, callback) =>
-          index === callback.findIndex((t) => t.idx === arr.idx),
-      );
-
-      // console.log('arrUnique', arrUnique);
-
-      if (value.length === 0) {
-        setOptionList([
-          ...arrUnique,
-          {
-            label: arr[0].label,
-            options: arr[0].optionsDeps,
-            idx: selectValue.idx,
-          },
-        ]);
-      }
-    }
-  };
-
-  useEffect(() => {
-    showOptionList();
-  }, [selectValue]);
-
-  const addSourceOptions = () => {
-    console.log('출처 카테고리 추가 API');
-    // openCreateModal();
-    setCount(count + 1);
-    if (sourceOptions.length < 5) {
-      setSourceOptions([...sourceOptions, count]);
-    }
-  };
-
-  const removeSourceOptions = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const target = event.currentTarget;
-    const id = target.parentElement?.parentElement?.id;
-    const arr = sourceOptions.filter((el) => el !== Number(id));
-    setSourceOptions(arr);
-
-    // console.log('부모 배열wldnjwlq el', arr);
-    console.log('부모 셀렉트selectValue el', id);
-    // 부모 셀렉트 삭제시 옵션셀렉트도 배열에서 삭제
-    const resetArr = optionList.filter((el) => el.idx !== Number(id));
-    // console.log('자식 셀렉트 삭제시resetArr el', resetArr);
-    setOptionList(resetArr);
-  };
-
-  console.log('전체optionList', optionList);
-
-  const modalData = {
-    title: '',
-    content: <SchoolInputModal />,
-    callback: () => {},
-  };
-
-  // 모달 연뒤
-  const openCreateModal = () => {
-    openModal(modalData);
   };
 
   const submitSave = () => {
@@ -175,106 +72,8 @@ export function ContentCreating() {
               출처<span>*</span>
             </strong>
             <SourceOptionWrap>
-              {sourceOptions.map((index) => (
-                <SelectList key={`${index} select`} id={index.toString()}>
-                  <li>
-                    {index === 0 ? (
-                      <Button
-                        width={'50px'}
-                        height={'30px'}
-                        fontSize={'15px'}
-                        $padding={'5px'}
-                        $filled
-                        cursor
-                        onClick={() => addSourceOptions()}
-                      >
-                        +
-                      </Button>
-                    ) : (
-                      <Button
-                        width={'50px'}
-                        height={'30px'}
-                        fontSize={'15px'}
-                        $padding={'5px'}
-                        $filled
-                        cursor
-                        onClick={(event) => removeSourceOptions(event)}
-                      >
-                        -
-                      </Button>
-                    )}
-                    <SelectWrapper>
-                      {selectCategory2.map((el) => (
-                        <>
-                          <Select
-                            width={'110px'}
-                            height={'30px'}
-                            defaultValue={el.label}
-                            key={el.label}
-                            options={el.options}
-                            onSelect={(event) => selectCategoryOption(event)}
-                          />
-                          <span>|</span>
-                        </>
-                      ))}
-
-                      {optionList &&
-                        optionList[index] &&
-                        optionList[index].options.map((el) => (
-                          <OptionWrap key={`${el.label} optionsDeps`}>
-                            <li>
-                              {el.type === 'input' && (
-                                <input placeholder={`${el.label}`} />
-                              )}
-                            </li>
-                            <li>
-                              {el.type === 'datepickup' && (
-                                <Button
-                                  width="90px"
-                                  height="30px"
-                                  fontSize="13px"
-                                  $normal
-                                  onClick={() => {}}
-                                  cursor
-                                >
-                                  기출일시
-                                </Button>
-                              )}
-                            </li>
-                            <li>
-                              {el.type === 'modal' && (
-                                <Button
-                                  width="90px"
-                                  height="30px"
-                                  fontSize="13px"
-                                  $normal
-                                  onClick={() => openCreateModal()}
-                                  cursor
-                                >
-                                  {el.label}
-                                </Button>
-                              )}
-                            </li>
-                            <li>
-                              {el.type === 'select' && (
-                                <Select
-                                  width={'110px'}
-                                  height={'30px'}
-                                  defaultValue={el.label}
-                                  key={el.label}
-                                  options={el.options as OptionsDepsProps[]}
-                                  onSelect={(event) =>
-                                    selectCategoryOption(event)
-                                  }
-                                />
-                              )}
-                            </li>
-                          </OptionWrap>
-                        ))}
-                    </SelectWrapper>
-                  </li>
-                </SelectList>
-              ))}
+              {/* 옵션 리스트 셀렉트 컴포넌트 */}
+              <OptionList />
             </SourceOptionWrap>
           </SelectListWrap>
           <SelectListWrap>
@@ -346,7 +145,7 @@ const ScrollWrap = styled.div`
 const SelectListWrap = styled.div`
   display: flex;
   /* align-items: center; */
-  padding: 0 10px;
+  padding: 0 15px;
 
   strong {
     min-width: 40px;
@@ -365,10 +164,10 @@ const SelectListWrap = styled.div`
   }
 
   &:last-child {
-    padding-bottom: 10px;
+    padding-bottom: 20px;
   }
   &:nth-child(1) {
-    padding-top: 10px;
+    padding-top: 20px;
   }
 `;
 
@@ -392,18 +191,6 @@ const SelectWrapper = styled.div`
   gap: 5px;
   align-items: center;
   color: ${COLOR.GRAY};
-`;
-const OptionWrap = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-
-  input {
-    height: 30px;
-    padding: 10px;
-    width: 120px;
-    border: 1px solid ${COLOR.BORDER_GRAY};
-    border-radius: 5px;
-  }
 `;
 
 const ContentListWrap = styled.div`
