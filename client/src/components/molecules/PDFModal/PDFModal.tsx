b0b12355d5946e21912cb122c30c86f7f76daca1
hyoseon <hskim1@olympiad.ac>
2024-06-19 12:01:05 +0900

    프린트 높이 조정

--- client/src/components/molecules/PDFModal/PDFModal.tsx
+++ client/src/components/molecules/PDFModal/PDFModal.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { SlOptionsVertical, SlPrinter } from 'react-icons/sl';
 import PerfectScrollbar from 'react-perfect-scrollbar';
@@ -8,8 +8,8 @@ import { styled } from 'styled-components';
 
 import { Icon, IconButton, ValueNone } from '../../../components/atom';
 import { MathViewer } from '../../../components/mathViewer';
-import { QuizListType } from '../../../types';
-import { COLOR } from '../../constants';
+import { ItemQuestionType, QuizListType } from '../../../types';
+import { A4_HEIGHT, A4_WIDTH, COLOR } from '../../constants';
 
 import { makePdf } from './makePDF';
 
@@ -20,15 +20,17 @@ type PDFModalProps = {
 export function PDFModal({ list }: PDFModalProps) {
   const [sortedList, setSortedList] = useState<QuizListType[]>(list);
   const pdf = makePdf();
+  const printDivRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
-  const printPDF = async () => {
-    await pdf.viewWithPdf();
-  };
+  // const printPDF = async () => {
+  //   await pdf.viewWithPdf();
+  // };
 
   return (
     <>
       <IconButtonWrapper>
-        <IconButton
+        {/* <IconButton
           $iconOlny
           $borderNone
           onClick={() => printPDF()}
@@ -37,40 +39,56 @@ export function PDFModal({ list }: PDFModalProps) {
           $padding="0px"
         >
           <SlPrinter />
-        </IconButton>
+        </IconButton> */}
+
+        <ReactToPrint
+          trigger={() => (
+            <SlPrinter style={{ fontSize: '20px', cursor: 'pointer' }} />
+          )}
+          content={() => printDivRef.current}
+        />
       </IconButtonWrapper>
 
       <Component>
         <ScrollWrapper>
           <PerfectScrollbar>
-            {sortedList.map((item) => (
-              <div key={`${item.idx} pdf list`} className="A4_paper">
-                <Contents>
+            <ContentsWrapper ref={printDivRef}>
+              {sortedList.map((item) => (
+                <MathViewerList
+                  key={`${item.idx} pdf list`}
+                  className="A4_paper"
+                  ref={containerRef}
+                >
                   {item.quizItemList.length ? (
-                    item.quizItemList.map((el) => (
-                      <div key={`${el?.code} quizItemList sortedList`}>
-                        {[
-                          'TITLE',
-                          'QUESTION',
-                          'EXAMPLE',
-                          'ANSWER',
-                          'TIP',
-                          'COMMENTARY',
-                          'HINT',
-                        ].includes(el?.type) &&
-                          el?.content && (
-                            <MathViewer data={el.content}></MathViewer>
-                          )}
-                      </div>
-                    ))
+                    <PrintBox>
+                      {item.quizItemList.map((el) => (
+                        <MathViewerWrapper
+                          key={`${el?.code} quizItemList sortedList`}
+                          className="row"
+                        >
+                          {[
+                            'TITLE',
+                            'QUESTION',
+                            'EXAMPLE',
+                            'ANSWER',
+                            'TIP',
+                            'COMMENTARY',
+                            'HINT',
+                          ].includes(el?.type) &&
+                            el?.content && (
+                              <MathViewer data={el.content}></MathViewer>
+                            )}
+                        </MathViewerWrapper>
+                      ))}
+                    </PrintBox>
                   ) : (
-                    <>
+                    <ValueNoneWrapper>
                       <ValueNone info="문항 데이터가 없습니다" textOnly />
-                    </>
+                    </ValueNoneWrapper>
                   )}
-                </Contents>
-              </div>
-            ))}
+                </MathViewerList>
+              ))}
+            </ContentsWrapper>
           </PerfectScrollbar>
         </ScrollWrapper>
       </Component>
@@ -92,27 +110,16 @@ const Component = styled.div<PDFModalStyleProps>`
   height: fit-content;
   width: 100%;
   max-height: calc(100vh - 100px);
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: ${COLOR.LIGHT_GRAY};
+  border-radius: 0 0 10px 10px;
   border-top: 1px solid ${COLOR.BORDER_BLUE};
   overflow: hidden;
-
-  .A4_paper {
-    height: 842px;
-    width: 595px;
-    margin: 30px;
-    background-color: white;
-    text-align: center;
-  }
-
-  .A4_paper > div {
-    margin: 30px;
-  }
 `;
 
 const IconButtonWrapper = styled.div`
   position: absolute;
   top: 40px;
-  right: calc(50% - 300px);
+  right: calc(50% - 380px);
   width: 30px;
   height: 30px;
 `;
@@ -121,9 +128,10 @@ const ScrollWrapper = styled.div`
   height: calc(100vh - 100px);
   width: 100%;
   background-color: ${COLOR.LIGHT_GRAY};
+  margin-bottom: 40px;
 
   .line {
-    border-top: 1px solid ${COLOR.BORDER_GRAY};
+    border-top: 1px solid ${COLOR.LIGHT_GRAY};
     margin: 10px 0;
 
     &.bottom_text {
@@ -133,6 +141,45 @@ const ScrollWrapper = styled.div`
   }
 `;
 
-const Contents = styled.p`
-  padding: 30px;
+const ContentsWrapper = styled.div`
+  max-height: 100vh;
 `;
+
+const PrintBox = styled.div`
+  width: ${`${A4_WIDTH / 3 - 15}px`};
+  /* border: 2px solid black; */
+  margin: 0 auto;
+  padding-top: 10px;
+  margin-right: -15px;
+`;
+
+const MathViewerWrapper = styled.div`
+  display: flex;
+  width: ${`${A4_WIDTH / 3 - 30}px`};
+  /* border: 2px solid blue; */
+  padding: 0 20px;
+  padding-top: 10px;
+  margin: 0 -15px;
+`;
+
+const MathViewerList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  background-color: white;
+  text-align: center;
+  width: ${`${A4_WIDTH / 3 - 30}px`};
+  height: ${`${A4_HEIGHT / 3 - 14}px`};
+  /* border: 1px solid red; */
+  margin: 20px;
+  &.A4_paper > div {
+    padding: 10px;
+  }
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+`;
+const ValueNoneWrapper = styled.div``;
