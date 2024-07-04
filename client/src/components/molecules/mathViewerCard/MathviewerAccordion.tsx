772e1ab05a91087ac6399adb1ef74cfd2659641c
yeondongChoe <ydchoi@olympiad.ac>
2024-07-04 16:18:53 +0900

    학습지 수정 데이터 불러오는 시점 수정, 학습지 리스트 갱신

--- client/src/components/molecules/mathViewerCard/MathviewerAccordion.tsx
+++ client/src/components/molecules/mathViewerCard/MathviewerAccordion.tsx
@@ -318,7 +318,7 @@ export function MathviewerAccordion({
     >
       <Component className={className} $componentHeight={componentHeight}>
         <div className="leftInfomation">
-          {isFavorite ? (
+          {/* {isFavorite ? (
             <Icon
               width={`18px`}
               src={`/images/icon/favorites_on.svg`}
@@ -332,7 +332,11 @@ export function MathviewerAccordion({
               onClick={favoriteQuizItem}
               cursor
             />
-          )}
+          )} */}
+          <LuBookmarkPlus
+            onClick={favoriteQuizItem}
+            style={{ width: '18px', cursor: 'pointer' }}
+          ></LuBookmarkPlus>
           <div>{category?.난이도 || 'N/A'}</div>
           <div>{category?.문항타입 || 'N/A'}</div>
           {!isSimilarQuiz && !isNewQuiz && equalScore === 2 && (
