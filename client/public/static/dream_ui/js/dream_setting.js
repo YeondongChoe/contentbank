8ebffd65884bc432781501fec8f9cce8c65e5844
hyoseon <hskim1@olympiad.ac>
2024-07-10 15:56:03 +0900

    에디터 파일 변경내역 저장

--- client/public/static/dream_ui/js/dream_setting.js
+++ client/public/static/dream_ui/js/dream_setting.js
@@ -1,8 +1,18 @@
-console.log('맨앞---- test dream_setting.js console log');
-var dream_url = 'dddd';
+// 업로드 다운로드 요청 url
+var dream_server_url = "http://43.201.205.140:40030";
 
 // 데이터 로드 화면 탭 개수 설정
 var tabCount = 5;
+
+// 즐겨찾기 개수
+var bookmarkCount = 9;
+
+// 편집기 종류(PC: true, tab: false);
+// var editorType = true;
+var editorType = false;
+
+// redo, undo 단계 설정
+var undo_redo_level = 10;
 
 // 다문항 저장 시 문제 단위 설정
 // 문제 분할 단위 p태그 class명 설정
@@ -18,31 +28,33 @@ var tabCount = 5;
 // CONCEPT(개념): tag_concept
 // TITLE(제목): tag_title
 // TIP(팁): tag_tip
-var divideKey = ['tag_bigcontent', 'tag_exam'];
+// GROUP(그룹): tag_group
+// var divideKey = ["tag_bigcontent", "tag_exam"];
+var divideKey = ["tag_group"];
+
+// 이미지 저장 설정 (1: 로컬, 2: ftp, 3: s3)
+var img_save_type = 1;
 
 // 수식 입력 버튼 누를때 출력되는 값 반환
 
-var onlyEQ = false;
 // 수식만 넣을때 수식을 넣는 공간의 class명
-var onlyEQNode = '';
+var onlyEQ = false;
+var onlyEQNode = "";
 var getEQData = () => {
-  const output = document.querySelector(`.${onlyEQNode}`);
-  // eslint-disable-next-line no-undef
+  // const output = document.querySelector(`.${onlyEQNode}`);
   iTeXEQ
     .insertEqn()
     .then((node) => {
-      // console.log(node.html);
+      openEQ();
+      console.log(node);
+      console.log(node.html);
       // result에 들어가는 값
       const result = node.html;
       output.innerHTML = result;
     })
     .then(() => {
-      // eslint-disable-next-line no-undef
       iTeXEQ.recoverynew(output);
       const target = output.querySelector(`.itexmath`);
-      // eslint-disable-next-line no-undef
-      target.addEventListener('click', iTeXEQ.eqn_click, false);
+      target.addEventListener("click", iTeXEQ.eqn_click, false);
     });
 };
-
-console.log('맨뒤--- test dream_setting.js console log');
