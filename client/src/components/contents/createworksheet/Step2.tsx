ef7eb26656654442763f170dbd6458105efd15a6
yeondongChoe <ydchoi@olympiad.ac>
2024-08-16 15:14:07 +0900

    전체 추가 버그 수정

--- client/src/components/contents/createworksheet/Step2.tsx
+++ client/src/components/contents/createworksheet/Step2.tsx
@@ -1826,7 +1826,6 @@ export function Step2() {
   const [similarPrevItems, setSimilarPrevItems] = useState<SimilarQuizList[]>(
     [],
   );
-  console.log('similarItems:', similarItems);
 
   // 유사문항 요청 api
   const postSimilarItems = async () => {
@@ -1908,7 +1907,22 @@ export function Step2() {
     if (newQuizItems && getItemCountData) {
       const allNewQuizItems = newQuizItems.quizList;
       if (initialItems.length + allNewQuizItems.length <= getItemCountData) {
-        setInitialItems((prevItems) => [...prevItems, ...allNewQuizItems]);
+        setInitialItems((prevItems) => {
+          const maxNum = prevItems.reduce((max, item) => {
+            return item.num && item.num > max ? item.num : max;
+          }, 0);
+          const updatedQuizItem = allNewQuizItems.map((item, index) => ({
+            ...item,
+            num: item.num ?? maxNum + 1 + index,
+          }));
+          const filteredQuizItem = updatedQuizItem.filter(
+            (updatedItem) =>
+              !initialItems.some(
+                (initialItem) => initialItem.code === updatedItem.code,
+              ),
+          );
+          return [...prevItems, ...filteredQuizItem];
+        });
         setNewQuizItems((prevItems) => {
           if (prevItems) {
             return {
@@ -1929,7 +1943,22 @@ export function Step2() {
     else if (favoriteQuestionList && getItemCountData) {
       const allNewQuizItems = favoriteQuestionList.quizList;
       if (initialItems.length + allNewQuizItems.length <= getItemCountData) {
-        setInitialItems((prevItems) => [...prevItems, ...allNewQuizItems]);
+        setInitialItems((prevItems) => {
+          const maxNum = prevItems.reduce((max, item) => {
+            return item.num && item.num > max ? item.num : max;
+          }, 0);
+          const updatedQuizItem = allNewQuizItems.map((item, index) => ({
+            ...item,
+            num: item.num ?? maxNum + 1 + index,
+          }));
+          const filteredQuizItem = updatedQuizItem.filter(
+            (updatedItem) =>
+              !initialItems.some(
+                (initialItem) => initialItem.code === updatedItem.code,
+              ),
+          );
+          return [...prevItems, ...filteredQuizItem];
+        });
         setFavoriteQuestionList((prevItems) => {
           if (prevItems) {
             return {
@@ -1968,7 +1997,14 @@ export function Step2() {
           } else {
             setInitialItems((prevItems) => {
               if (prevItems) {
-                return [...prevItems, selectedQuizItem];
+                const maxNum = prevItems.reduce((max, item) => {
+                  return item.num && item.num > max ? item.num : max;
+                }, 0);
+                const updatedQuizItem = {
+                  ...selectedQuizItem,
+                  num: selectedQuizItem.num ?? maxNum + 1,
+                };
+                return [...prevItems, updatedQuizItem];
               }
               return [selectedQuizItem]; // 초기 상태 설정
             });
