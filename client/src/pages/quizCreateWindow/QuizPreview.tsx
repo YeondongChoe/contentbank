3882a92afd692a5870bc72de772eb3be759dd433
hyoseon <hskim1@olympiad.ac>
2024-10-16 15:36:35 +0900

    불필요 코드 삭제

--- client/src/pages/quizCreateWindow/QuizPreview.tsx
+++ client/src/pages/quizCreateWindow/QuizPreview.tsx
@@ -43,80 +43,11 @@ export function QuizPreview() {
     }
   }, [quizData]);
 
-  // useEffect(() => {
-  //   console.log(', quizDataTotalValue', quizDataTotalValue);
-  // }, [quizDataTotalValue]);
+  useEffect(() => {}, []);
 
   useEffect(() => {
-    // const queryParams = new URLSearchParams(location.search);
-    // const parseJson = (str: string | null) => {
-    //   try {
-    //     return str ? JSON.parse(str) : null;
-    //   } catch (e) {
-    //     console.error('Parsing error on', str);
-    //     return null;
-    //   }
-    // };
-    // const quizData: QuizListType = {
-    //   code: queryParams.get('code') || '',
-    //   createdAt: queryParams.get('createdAt') || '',
-    //   createdBy: queryParams.get('createdBy') || '',
-    //   idx: parseInt(queryParams.get('idx') || '0', 10),
-    //   isDelete: queryParams.get('isDelete') === 'true',
-    //   isUse: queryParams.get('isUse') === 'true',
-    //   isFavorite: queryParams.get('isFavorite') === 'true',
-    //   lastArticle: parseJson(queryParams.get('lastArticle')),
-    //   lastModifiedAt: queryParams.get('lastModifiedAt') || '',
-    //   lastModifiedBy: queryParams.get('lastModifiedBy') || '',
-    //   type: queryParams.get('type') || '',
-    //   userKey: queryParams.get('userKey') || '',
-    //   quizCategoryList: parseJson(queryParams.get('quizCategoryList')) || [],
-    //   quizItemList: parseJson(queryParams.get('quizItemList')) || [],
-    //   quizList: parseJson(queryParams.get('quizList')) || [],
-    // };
-    // }
-  }, []);
-
-  //iframe 데이터 통신시
-  const receiveMessage = (event: any) => {
-    // 불필요한 react-devtools 메세지 이벤트 차단
-    if (/^react-devtools/gi.test(event.data.source)) {
-      return;
-    }
-    const { type } = event.data;
-    // if (type === 'BOOL')
-  };
-
-  // 부모 로컬스토리지에서 데이터 가져오기
-  // const getLocalData = () => {
-  //   const data = localStorage.getItem('sendData');
-  //   let sendData;
-  //   if (data) sendData = JSON.parse(data);
-
-  //   console.log('데이터 조회', sendData && sendData.data);
-  //   setList(sendData.data);
-  //   // setSortedList() // 문항데이터 넣기
-
-  //   // 로컬스토리지 값 다받은 뒤 초기화
-  //   window.opener.localStorage.clear();
-  // };
-
-  // useEffect(() => {
-  //   getLocalData();
-  // }, []);
-
-  // useEffect(() => {
-  //   quizDataRefetch();
-  // }, [list]);
-
-  // useEffect(() => {
-  //   if (quizData) setSortedList(quizData.quizList);
-  // }, [quizData]);
-
-  useEffect(() => {
-    console.log('sortedList----------, ', sortedList);
+    // console.log('sortedList----------, ', sortedList);
   }, [sortedList]);
-  console.log('sortedList----------, ', sortedList);
 
   return (
     <Container>
